664ab677937aa04aef1206c9245a9ff1c6094df6
Héctor Alonso Salinas García <ha_sg@outlook.com>
2025-06-23 14:23:33 -0600

    Removed hello func

--- c_src/sensors_nif.c
+++ c_src/sensors_nif.c
@@ -54,23 +54,6 @@ static ERL_NIF_TERM get_cpu_temp_fahrenheit_nif(ErlNifEnv *env, int argc,
   return enif_make_double(env, get_cpu_temp_fahrenheit());
 }
 
-static ERL_NIF_TERM hello_nif(ErlNifEnv *env, int argc,
-                              const ERL_NIF_TERM argv[]) {
-  const char *str = "¡Hello, 🌍!"; // Your UTF-8 string
-  size_t len = strlen(str);        // Get length in bytes
-
-  ErlNifBinary bin;
-  if (!enif_alloc_binary(len, &bin)) {
-    return enif_make_atom(env, "error_allocating_binary");
-  }
-
-  get_cpu_temp_celcius();
-
-  memcpy(bin.data, str, len);
-
-  return enif_make_binary(env, &bin);
-}
-
 // --- NIF Lifecycle ---
 
 static int load(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM info) {
@@ -85,7 +68,6 @@ static void unload(ErlNifEnv *env, void *priv_data) { sensors_cleanup(); }
 
 // --- NIF Exports ---
 static ErlNifFunc nif_funcs[] = {
-    {"hello", 0, hello_nif},
     {"get_cpu_temp_celcius", 0, get_cpu_temp_celcius_nif},
     {"get_cpu_temp_fahrenheit", 0, get_cpu_temp_fahrenheit_nif}
 };
--- lib/exkl/sensors_nif.ex
+++ lib/exkl/sensors_nif.ex
@@ -1,11 +1,10 @@
 defmodule Exkl.SensorsNif do
   @on_load :init
   @nif_path Path.join(:code.priv_dir(:exkl), "/nifs/sensors_nif")
-  @nifs [hello: 0, get_cpu_temp_celcius: 0, get_cpu_temp_fahrenheit: 0]
+  @nifs [get_cpu_temp_celcius: 0, get_cpu_temp_fahrenheit: 0]
 
   def init, do: :erlang.load_nif(@nif_path, 0)
 
-  def hello, do: load_error(:sensors_nif_library_not_loaded)
   def get_cpu_temp_celcius, do: load_error(:sensors_nif_library_not_loaded)
   def get_cpu_temp_fahrenheit, do: load_error(:sensors_nif_library_not_loaded)
 
