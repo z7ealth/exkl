d0a3379157cc143a90aa60fbf0d87906f7838c1a
Héctor Alonso Salinas García <ha_sg@outlook.com>
2025-06-23 08:50:44 -0600

    Show webview

--- lib/exkl/main_window.ex
+++ lib/exkl/main_window.ex
@@ -2,7 +2,8 @@ defmodule Exkl.MainWindow do
   @behaviour :wx_object
 
   @title "EXKL"
-  @size {600, 600}
+  @size {1200, 800}
+  @icon_path Path.join(:code.priv_dir(:exkl), "static/images/deepcool.bmp")
 
   def start_link() do
     :wx_object.start_link(__MODULE__, [], [])
@@ -12,13 +13,14 @@ defmodule Exkl.MainWindow do
     wx = :wx.new()
     frame = :wxFrame.new(wx, -1, @title, size: @size)
 
-    task_bar = :wxTaskBarIcon.new()
-    :wxTaskBarIcon.setIcon(task_bar, build_icon())
+    task_bar = build_taskbar()
+    web_view = build_webview(frame)
 
+    :wxFrame.setIcon(frame, build_icon())
     :wxFrame.show(frame)
     :wxFrame.maximize(frame)
 
-    state = %{frame: frame}
+    state = %{task_bar: task_bar, web_view: web_view}
     {frame, state}
   end
 
@@ -28,9 +30,14 @@ defmodule Exkl.MainWindow do
     {:noreply, state}
   end
 
-  defp build_icon do
-    icon_path = Path.join(:code.priv_dir(:exkl), "static/images/deepcool.bmp")
-    dbg(icon_path)
-    :wxIcon.new(icon_path)
+  defp build_webview(frame) do
+    :wxWebView.new(frame, -1, url: "http://localhost:4000")
   end
+
+  defp build_taskbar do
+    task_bar = :wxTaskBarIcon.new()
+
+    :wxTaskBarIcon.setIcon(task_bar, build_icon())
+  end
+  defp build_icon, do: :wxIcon.new(@icon_path)
 end
