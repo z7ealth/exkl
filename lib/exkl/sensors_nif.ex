0c3af6258516b9c5519a00960920fb1e302c4aef
Héctor Alonso Salinas García <ha_sg@outlook.com>
2025-07-11 16:23:57 -0600

    Renamed celcius -> celsius

--- lib/exkl/sensors_nif.ex
+++ lib/exkl/sensors_nif.ex
@@ -1,11 +1,11 @@
 defmodule Exkl.SensorsNif do
   @on_load :init
   @nif_path Path.join(:code.priv_dir(:exkl), "/nifs/sensors_nif")
-  @nifs [get_cpu_temp_celcius: 0, get_cpu_temp_fahrenheit: 0]
+  @nifs [get_cpu_temp_celsius: 0, get_cpu_temp_fahrenheit: 0]
 
   def init, do: :erlang.load_nif(@nif_path, 0)
 
-  def get_cpu_temp_celcius, do: load_error(:sensors_nif_library_not_loaded)
+  def get_cpu_temp_celsius, do: load_error(:sensors_nif_library_not_loaded)
   def get_cpu_temp_fahrenheit, do: load_error(:sensors_nif_library_not_loaded)
 
   defp load_error(error) do
